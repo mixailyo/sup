cee349870612c2ee9a72d36a49617e74e5e2550d
mixailyo <67204306+mixailyo@users.noreply.github.com>
2022-08-04 09:38:00 +0300

    Добавлен submit в формы поиска

--- source/js/blocks/search-form.js
+++ source/js/blocks/search-form.js
@@ -5,21 +5,27 @@ function searchForm() {
 
   if (searchForms.length) {
     searchForms.forEach((form) => {
+      let searchFormSubmitBtn = form.querySelector('.search__form-submit');
+
       form.addEventListener('submit', (e) => {
         e.preventDefault();
-      })
+      });
+
+      searchFormSubmitBtn.addEventListener('click', () => {
+        form.submit();
+      });
 
       searchFormFiltersBtn.addEventListener('click', () => {
-        form.classList.add('search__form--active')
-        document.body.classList.add('scroll-lock')
-      })
+        form.classList.add('search__form--active');
+        document.body.classList.add('scroll-lock');
+      });
 
       searchFormBackBtn.addEventListener('click', () => {
-        form.classList.remove('search__form--active')
-        document.body.classList.remove('scroll-lock')
-      })
-    })
+        form.classList.remove('search__form--active');
+        document.body.classList.remove('scroll-lock');
+      });
+    });
   }
 }
 
-export { searchForm }
+export {searchForm};
